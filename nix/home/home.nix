d946d91fd9a192e6dc14683d99611b9cfc296bad
SakuyaAshford <182094416+SakuyaAshford@users.noreply.github.com>
2025-01-14 11:46:21 +0700

    add oh-my-posh

--- nix/home/home.nix
+++ nix/home/home.nix
@@ -126,6 +126,7 @@
   programs = {
     fzf = import ./fzf.nix { inherit pkgs; };
     neovim = import ./neovim.nix { inherit config pkgs; };
+    oh-my-posh = import ./oh-my-posh.nix { inherit pkgs;};
     zsh = import ./zsh.nix { inherit config pkgs lib; };
     git = import ./git.nix { inherit config pkgs; };
     zoxide = import ./zoxide.nix { inherit pkgs; };
