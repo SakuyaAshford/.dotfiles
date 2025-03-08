d28ebd8ba9265963394d00ee12cf4a0e13fc59e3
SakuyaAshford <182094416+SakuyaAshford@users.noreply.github.com>
2025-03-08 16:27:12 +0700

    configure nvim configuration with lua

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,9 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.scrolloff = 8
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.cmd("colorscheme desert")
--- nix/home/neovim.nix
+++ nix/home/neovim.nix
@@ -4,7 +4,7 @@
   inputs,
   ...
 }: {
-  imports = [inputs.nvf.homeManagerModules.default];
+  imports = [ inputs.nvf.homeManagerModules.default ];
 
   programs.neovim = {
     enable = true;
@@ -12,68 +12,12 @@
     vimAlias = true;
   };
 
+  # Creates a symlink from the Neovim config directory (e.g. ~/.config/nvim)
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink "/home/sakuya/.dotfiles/.config/nvim";
 
-
   programs.nvf = {
-    enable = false;
-    enableManpages = true;
-    settings.vim = {
-      vimAlias = true;
-      viAlias = true;
-
-      theme = {
-        enable = true;
-        name = "catppuccin";
-        style = "frappe";
-        transparent = true;
-      };
-      telescope.enable = true;
-
-      spellcheck = {
-        enable = true;
-      };
-      lsp = {
-        formatOnSave = true;
-        lspkind.enable = false;
-        lightbulb.enable = true;
-        lspsaga.enable = false;
-        trouble.enable = true;
-        lspSignature.enable = true;
-        otter-nvim.enable = false;
-        lsplines.enable = false;
-        nvim-docs-view.enable = false;
-      };
-
-      languages = {
-        enableLSP = true;
-        enableFormat = true;
-        enableTreesitter = true;
-        enableExtraDiagnostics = true;
-
-        nix.enable = true;
-        python.enable = true;
-        rust.enable = true;
-      };
-
-      visuals = {
-        nvim-cursorline.enable = true;
-        cinnamon-nvim.enable = true;
-        fidget-nvim.enable = true;
-
-        highlight-undo.enable = true;
-        indent-blankline.enable = true;
-      };
-
-      statusline = {
-        lualine = {
-          enable = true;
-          theme = "catppuccin";
-        };
-      };
-
-      autopairs.nvim-autopairs.enable = true;
-    };
+    enable = false; # enabling it will change the config to ~/.config/nvf
   };
 }
+
