818036e15b725733da31309e14c4547c07d4ed84
SakuyaAshford <182094416+SakuyaAshford@users.noreply.github.com>
2025-03-19 16:31:28 +0700

    config: add multiple plugins for neovim

--- .config/nvim/lua/plugins/bullets-nvim.lua
+++ .config/nvim/lua/plugins/bullets-nvim.lua
@@ -1,7 +1,8 @@
 return {
   {
     "dkarter/bullets.vim",
-    event = "BufReadPre", 
+    enabled = false,
+    event = "BufReadPre",
     config = function()
       -- Configure the filetypes where bullets.vim should be active.
       vim.g.bullets_enabled_filetypes = { "markdown", "text", "gitcommit" }
