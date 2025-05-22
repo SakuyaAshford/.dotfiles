6485a95f1027ad391f3e7147318b968823c81015
SakuyaAshford <182094416+SakuyaAshford@users.noreply.github.com>
2025-05-22 16:17:41 +0700

    feat: add lualine plugin

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        event = "VeryLazy",
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = '|',
+                section_separators = { left = '', right = '' },
+            },
+        },
+        init = function()
+            vim.g.lualine_laststatus = vim.o.laststatus
+            if vim.fn.argc(-1) > 0 then
+                -- set an empty statusline till lualine loads
+                vim.o.statusline = " "
+            else
+                -- hide the statusline on the starter page
+                vim.o.laststatus = 0
+            end
+        end,
+    },
+}
+
