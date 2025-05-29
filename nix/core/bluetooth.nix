9dc24e39a56c2a383c8b91282be4d6f9d0e6d8cd
SakuyaAshford <182094416+SakuyaAshford@users.noreply.github.com>
2025-05-29 10:10:17 +0700

    config: fix bluetooth problem

--- nix/core/bluetooth.nix
+++ nix/core/bluetooth.nix
@@ -3,5 +3,8 @@
   hardware.bluetooth = {
     enable = true;
     # autoEnable = true; # Automatically enable Bluetooth on boot
+    settings.general = {
+        ControllerMode = "dual";
+    };
   };
 }
